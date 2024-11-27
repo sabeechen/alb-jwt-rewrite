79550d2a3f88078a55258c00ae54bb50c3651684
Stephen Beechen <sbeechen@pollyex.com>
2024-11-27 08:23:39 -0700

    New version

--- jwt_decoder.lua
+++ jwt_decoder.lua
@@ -1,43 +1,14 @@
 local jwt = require "resty.jwt"
 local cjson = require "cjson"
-local resty_rsa = require "resty.rsa"
-local str = require "resty.string"
 
-local rsa_public_key, rsa_private_key
 
-function generate_key_pair()
-    local rsa_config = {
-        key_type = resty_rsa.KEY_TYPE.PRIVATE,
-        key_size = 2048,
-    }
-    local rsa_public_key, rsa_priv_key, err = resty_rsa:generate_rsa_keys(2048)
-    if not rsa_priv_key then
-        ngx.log(ngx.ERR, "Failed to generate private key: ", err)
-        return nil
-    end
-    return rsa_priv_key, rsa_public_key
-end
-
-function init_keys()
-    if not rsa_private_key then
-        local priv_key, pub_key = generate_key_pair()
-        if not priv_key then
-            return false
-        end
-        rsa_private_key, rsa_public_key = priv_key, pub_key
-    end
-    return true
-end
 
 function decode_and_set_headers()
-    if not init_keys() then
-        return
-    end
-
     local jwt_token = ngx.req.get_headers()["x-amzn-oidc-data"]
     
     if not jwt_token then
         ngx.log(ngx.ERR, "No JWT token found in x-amzn-oidc-data header")
+        ngx.req.set_header("Jwt-Parse", "NoToken")
         return
     end
 
@@ -45,6 +16,7 @@ function decode_and_set_headers()
     
     if not jwt_obj.valid then
         ngx.log(ngx.ERR, "Invalid JWT token: " .. jwt_obj.reason)
+        ngx.req.set_header("Jwt-Parse", "InvalidToken")
         return
     end
 
@@ -52,38 +24,17 @@ function decode_and_set_headers()
 
     -- Re-encode the JWT with our own key
     local claims = jwt_obj.payload
-    claims.iss = "our-custom-issuer"  -- Set your custom issuer
-    claims.iat = ngx.time()
-    claims.exp = ngx.time() + 3600  -- 1 hour expiration
-
-    local new_jwt, err = jwt:sign(rsa_private_key, {
-        header = {typ = "JWT", alg = "RS256"},
-        payload = claims
-    })
-
-    if not new_jwt then
-        ngx.log(ngx.ERR, "Failed to create new JWT: ", err)
-        return
-    end
-
-    -- Set the new JWT as a header
-    ngx.req.set_header("X-Custom-JWT", new_jwt)
-    ngx.log(ngx.ERR, "new JWT: ", new_jwt)
 
     -- Set individual claims as headers
     for k, v in pairs(claims) do
         ngx.req.set_header("X-User-" .. k, tostring(v))
     end
-end
 
-function get_public_key()
-    init_keys()
-    return rsa_public_key
+    ngx.req.set_header("Jwt-Parse", "Success")
 end
 
 decode_and_set_headers()
 
 return {
     decode_and_set_headers = decode_and_set_headers,
-    get_public_key = get_public_key
 }
